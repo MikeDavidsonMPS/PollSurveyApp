c243d2750571ab9cd701646233d92c38a8824aee
Jared Wooten <pancakeparfait@gmail.com>
2021-08-04 21:45:39 -0400

    Added Cors to all routes (except /token)

--- PollSurveyApp.API.NET/App_Start/WebApiConfig.cs
+++ PollSurveyApp.API.NET/App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 
 namespace PollSurveyApp.API.NET
 {
@@ -12,11 +8,13 @@ namespace PollSurveyApp.API.NET
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
