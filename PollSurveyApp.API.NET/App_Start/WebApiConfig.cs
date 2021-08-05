f3eba650a4d9a2e1b4b87d799add6af81a4d107b
Mike Davidson <meridianproservice@outlook.com>
2021-08-05 09:33:45 -0400

    Cor

--- PollSurveyApp.API.NET/App_Start/WebApiConfig.cs
+++ PollSurveyApp.API.NET/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -12,11 +13,13 @@ namespace PollSurveyApp.API.NET
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
             // Web API routes
             config.MapHttpAttributeRoutes();
 
